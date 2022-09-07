d37505c6de1bcac0e55c3ea52cc88c9ddce82fa3
idleberg <git@idleberg.com>
2022-09-07 11:38:57 +0200

    first commit

---
+++ Formula/scummc.rb
@@ -0,0 +1,37 @@
+class Scummc < Formula
+  desc "Set of tools allowing to create SCUMM games"
+  homepage "https://github.com/AlbanBedel/scummc#readme"
+  url "https://github.com/AlbanBedel/scummc/archive/v0.2.1.tar.gz"
+  sha256 "1d9b412d7ab6197495589a19a4745f58b35a002e26cea21bf6874a7ae4f1fd2e"
+
+  depends_on "bison" => :build
+  depends_on "freetype" => :build
+  depends_on "gtk+" => :build
+  depends_on "readline" => :build
+
+  def install
+    # Requires Bison v2.7 or later, but all supported macOS versions
+    # ship with v2.3
+    system "./configure", "--bison", "#{Formula["bison"].opt_bin}/bison"
+    system "make"
+
+    # There currently is no way to specify a build folder
+    # Default naming scheme: build.<HOSTNAME>.<COMPILER_VERSION>
+    bin.install Dir["build.*/**/scc"]
+    bin.install Dir["build.*/**/sld"]
+    bin.install Dir["build.*/**/cost"]
+    bin.install Dir["build.*/**/char"]
+    bin.install Dir["build.*/**/soun"]
+    bin.install Dir["build.*/**/midi"]
+  end
+
+  test do
+    # TODO: Write meaningful tests
+    assert FileTest.exists?("#{bin}/scc")
+    assert FileTest.exists?("#{bin}/sld")
+    assert FileTest.exists?("#{bin}/cost")
+    assert FileTest.exists?("#{bin}/char")
+    assert FileTest.exists?("#{bin}/soun")
+    assert FileTest.exists?("#{bin}/midi")
+  end
+end
---
+++ README.md
@@ -0,0 +1,28 @@
+# homebrew-scummc
+
+[![BSD 2-Clause License](https://flat.badgen.net/badge/license/BSD%202-Clause/blue)](https://opensource.org/licenses/BSD-2-Clause)
+[![Latest Release](https://flat.badgen.net/github/release/idleberg/homebrew-scummc)](https://github.com/idleberg/homebrew-scummc/releases)
+
+Homebrew tap for [ScummC](https://github.com/AlbanBedel/scummc)
+
+## Prerequisites
+
+Make sure that [Homebrew](https://brew.sh/) is installed with `brew` in your `PATH` environmental variable
+
+## Installation
+
+Tap this repository
+
+```sh
+$ brew tap idleberg/scummc
+```
+
+You can now install ScummC as you would any other Homebrew formula
+
+```sh
+$ brew install scummc
+```
+
+## License
+
+This work is licensed under the [BSD 2-Clause License](LICENSE)
